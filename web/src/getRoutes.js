1c53b62beda923a75edb9a15bb053be3df2e80cd
Stephen Handley <stephen.handley@gmail.com>
2022-04-05 11:58:41 -0700

    flatten routes

--- web/src/getRoutes.js
+++
@@ -1,8 +0,0 @@
-import AppRoutes from './Routes'
-import BrandRoutes from './BrandRoutes'
-
-export default function getRoutes() {
-  const { hostname } = window.location
-  const isApp = hostname === 'app.localhost'
-  return isApp ? AppRoutes : BrandRoutes
-}
